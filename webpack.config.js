3804cf88f05d1ab7d3e5f107e1705656de3b626b
Alexander <alex.kadoh@mail.ru>
2022-04-02 17:05:46 +0700

    add css and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './src/index.js',
-        secondary: './src/secondary.js',
+        main: './index.js',
+        secondary: './secondary.js',
     },
     output: {
         filename: '[name].[contenthash].js',
@@ -14,8 +15,16 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html'
+            template: './index.html'
         }),
         new CleanWebpackPlugin(),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader'],
+            }
+        ]
+    }
 }
