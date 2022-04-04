fc064944f1370310c978969c0c6e123fba12abde
Alexander <alex.kadoh@mail.ru>
2022-04-04 17:07:08 +0700

    add file-loader for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,14 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader','css-loader'],
+                use: ['style-loader','css-loader']
             },
             {
                 test: /\.(png | jpg | svg | gif | jpeg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf | woff | woff2 | eot)$/,
                 use: ['file-loader']
             }
         ]
